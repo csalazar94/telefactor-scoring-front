5fc6c259064529ce6433501b5104008e79f8e9e2
Camilo Salazar <csalazar@jumpitt.com>
2023-11-19 20:22:02 -0300

    🐛 prefix env with NEXT_PUBLIC_

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 const configuration = {
-  backend: `${process.env.BACKEND_PROTOCOL}${process.env.BACKEND_HOST}${
-    process.env.BACKEND_PORT ? ":" + process.env.BACKEND_PORT : ""
+  backend: `${process.env.NEXT_PUBLIC_BACKEND_PROTOCOL}${process.env.NEXT_PUBLIC_BACKEND_HOST}${
+    process.env.NEXT_PUBLIC_BACKEND_PORT ? ":" + process.env.NEXT_PUBLIC_BACKEND_PORT : ""
   }`,
 };
 
